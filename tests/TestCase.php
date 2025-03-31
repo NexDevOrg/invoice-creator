b7b36218324dcd29cfd216afb42ce84bb9033819
Quinten van der Laan <quinten1vanderlaan@outlook.com>
2025-03-31 11:07:28 +0200

    feat: enhance development setup with testing and autoload configurations
    - Added autoload-dev section in composer.json for PSR-4 autoloading of test classes.
    - Included Pest and Orchestra Testbench as development dependencies for improved testing capabilities.
    - Updated scripts to include a command for running Pest tests.
    - Configured allow-plugins for Pest plugin to enable additional testing features.

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace NexDev\InvoiceCreator\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+}
